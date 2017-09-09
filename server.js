3705efa113ef3cc8cccf73c470afe3375099d456
Brad Brandhorst <bradbrandhorst@gmail.com>
2017-09-09 14:14:18 -0400

    passport auth

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
+var express = require('express');
+var app = express();
+var passport = require('passport');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var env = require('dotenv').load();
+var exphbs = require('express-handlebars');
+
 
 //Models
 var models = require("./app/models");
+
+//Routes
+var authRoute = require('./app/routes/auth.js')(app);
  
 //Sync Database
 models.sequelize.sync().then(function() {
@@ -13,3 +23,10 @@ models.sequelize.sync().then(function() {
     console.log(err, "Something went wrong with the Database Update!")
  
 });
+
+//For Handlebars
+app.set('views', './app/views');
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
