499f746f40f765a546afed97451d56691f6454b4
Li Chen <cxli@email.unc.edu>
2017-09-11 18:41:18 -0400

    updated

--- controllers/gift_controller.js
+++ controllers/gift_controller.js
@@ -13,20 +13,20 @@ request({
   
 });
 
-
-function grouponItems(category, count, callback){
+//app.post ()
+function grouponItems(category, count, callback){//express handler - get
 	var url='https://partner-api.groupon.com/deals.json?tsToken=US_AFF_0_201236_212556_0&filters=category:'+category+'&offset=0&limit='+count
 	console.log(url)
 	request({
 	  url:url
 	}, function(err, res, body) {
 		var x= JSON.parse(body)
-		console.log(x.deals);
+		//console.log(x.deals);
 	  var respond=[];
 	  for(var i=0; i<x["deals"].length;i++){
-	  	respond.push({dealurl: x.deals[i].dealUrl, image: x.deals[i].smallImageUrl});
+	  	respond.push({dealTitle: x.deals[i].title, dealurl: x.deals[i].dealUrl, image: x.deals[i].smallImageUrl});
 	  }
-	  callback(respond);
+	  callback(respond);//res.render({name: respond}) 
 	});
 
 
