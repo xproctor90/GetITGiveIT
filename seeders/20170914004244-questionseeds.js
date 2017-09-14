7686da1a0edd244bc4e4a3e1f8a3087d5e9491bf
Brad Brandhorst <bradbrandhorst@gmail.com>
2017-09-13 21:35:31 -0400

    fully seeded I hope

---
+++ seeders/20170914004244-questionseeds.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('surveyQuestions', [{
+        id: 1,
+        question: "What do you like?",
+        createdAt: new Date(),
+        updatedAt: new Date()
+       }],
+       {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('surveyQuestions', [{
+         id: 1
+    }],
+    {});
+  }
+};
