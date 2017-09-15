36711775adad896bd19fe242965b4c5829d7c0d8
Brad Brandhorst <bradbrandhorst@gmail.com>
2017-09-15 16:27:56 -0400

    user controller and routing

---
+++ controllers/usercontroller.js
@@ -0,0 +1,28 @@
+var express = require("express");
+
+var router = express.Router();
+
+var users = require("../app/models/users.js");
+
+router.get("/", function(req, res) {
+  users.all(function(data) {
+    var hbsObject = {
+      users: data
+    };
+    console.log(hbsObject);
+    res.render("users", hbsObject);
+  });
+});
+
+router.post("/survey", function(req, res) {
+  users.create([
+    "firstname", "lastname", "username", "about", "email", "password"
+  ], [
+    req.body.firstname, req.body.lastname, req.body.username, req.body.about, req.body.email, req.body.password
+  ], function() {
+    res.redirect("/");
+  });
+});
+
+
+module.exports = router;
